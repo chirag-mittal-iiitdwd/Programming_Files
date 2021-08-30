d3ae5b37ff609b8166bb66adb35c79cd6a8e8833
Chirag Mittal <chirag.workid@gmail.com>
2021-08-31 00:40:41 +0530

    iniital commit 2

--- JavaFiles/FirstProject/src/com/company/Literals.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Literals {
-    public static void main(String[] args) {
-        byte age=34;
-        System.out.println(age);
-    }
-}
--- JavaFiles/FirstProject/src/com/company/percentageCalculator.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class percentageCalculator {
-    public static void main(String[] args) {
-    }
-}
