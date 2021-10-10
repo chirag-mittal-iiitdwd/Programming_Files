b7f73af22fc15696a1076839deefe22b32dd64bb
Chirag Mittal <chirag.workid@gmail.com>
2021-10-10 14:42:48 +0530

    longest arithmatic subarray

--- Gate Course/Algorithms/longestArithmaticSubarray.cpp
+++ Gate Course/Algorithms/longestArithmaticSubarray.cpp
@@ -2,11 +2,30 @@
 using namespace std;
 
 int longestArithmaticSubarray(int arr[],int n){
-    int longest=0;
-    for(int i=0;i<n;i++){
-        longest=0;
-        for(int j=i;j<n;j++){
-            
+    // In Constraints we are given that 2<=N
+    int maxAns;
+    int currentDiff=arr[0]-arr[1];
+    int currentAns=2;
+    maxAns=currentAns;
+    for(int i=1;i<n-1;i++){
+        if(arr[i]-arr[i+1]==currentDiff){
+            currentAns++;
         }
+        else{
+            currentDiff=arr[i]-arr[i+1];
+            currentAns=2;
+        }
+        maxAns=max(maxAns,currentAns);
     }
+    return maxAns;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<longestArithmaticSubarray(arr,n);
 }
