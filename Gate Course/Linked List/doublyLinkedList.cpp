ede671f1def2f84fa426272b33161186c5ec46ac
Chirag Mittal <chirag.workid@gmail.com>
2021-10-25 16:31:07 +0530

    doubly linked list

--- Gate Course/Linked List/doublyLinkedList.cpp
+++ Gate Course/Linked List/doublyLinkedList.cpp
@@ -16,11 +16,14 @@ class node{
 void insertAtHead(node*&head,int val){
     node* n=new node(val);
     n->next=head;
-    head->prev=n;
+    if(head!=NULL){
+        head->prev=n;
+    }
+    head=n;
 }
 
 void insertAtTail(node*&head,int val){
-    if(head=NULL){
+    if(head==NULL){
         insertAtHead(head,val);
         return;
     }
@@ -39,6 +42,7 @@ void deleteAtHead(node*&head){
     }
     node*teoDelete=head;
     head->next->prev=NULL;
+    head=head->next;
     // head->next=NULL;
     delete teoDelete;
 }
@@ -48,5 +52,37 @@ void deleteAtTail(node*&head){
         deleteAtHead(head);
         return;
     }
-    
+    node* temp=head;
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    node*toDelete=temp->next;
+    temp->next=NULL;
+    delete toDelete;
+}
+
+void display(node*head){
+    node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node*head=NULL;
+    insertAtHead(head,1);
+    insertAtHead(head,0);
+    display(head);
+
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    display(head);
+
+    deleteAtHead(head);
+    display(head);
+
+    deleteAtTail(head);
+    display(head);
 }
